744488f2f6e83eeeadb391b2d15893c72f0def86
Akshay0809 <96840620+Akshay0809@users.noreply.github.com>
2022-05-14 09:14:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title("Iris Classifier- AKSHAY")
+st.title("Iris Classifier- T AKSHAY")
                                           
 sl = st.slider('Sepal Length', 4.3, 7.9, 0.5)               
 sw = st.slider('Sepal Width', 2.0, 4.4, 0.5)
